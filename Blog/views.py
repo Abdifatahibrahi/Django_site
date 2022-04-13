e7ed34aaa980beed069034a355186ff969906d99
Abdifatah <alsidiq099@gmail.com>
2022-04-13 23:15:56 +0300

    first commit

---
+++ Blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+def starting_page(request):
+    return render(request, "blog/index.html")
+
+def posts(request):
+    pass
+
+
+def post_detail(request):
+    pass
